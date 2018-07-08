0b1b01240d9a862ff499a4bb4500a8f5887552b7
Liza <lizalepage@gmail.com>
2018-07-08 11:50:29 +0000

    private wikis enabled

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -1,7 +1,9 @@
 class DowngradeController < ApplicationController
   def create
     
+    
     current_user.update_attribute(:role, 'standard')
+    current_user.wikis.update_all(private: false)
     flash[:notice] = "You have successfully downgraded your account."
         
     redirect_to root_path
